a648e7e3b494412a52dc650ffe295595e191984e
fatimahanimashaun <fatimahanimashaun@womentechsters.org>
2025-03-16 02:07:36 +0100

    Assignment trial

--- First_test.py
+++ First_test.py
@@ -11,3 +11,11 @@ driver.find_element(By.ID, "user-name").send_keys("standard_user")
 driver.find_element(By.ID, "password").send_keys("secret_sauce")
 driver.find_element(By.ID, "login-button").click()
 time.sleep(5)
+
+driver.find_element(By.ID, "add-to-cart-sauce-labs-backpack").click()
+driver.find_element(By.ID, "add-to-cart-sauce-labs-bike-light").click()
+driver.find_element(By.ID, "add-to-cart-sauce-labs-bolt-t-shirt").click()
+
+driver.find_element(By.ID, "shopping_cart_container").click()
+time.sleep(5)
+
