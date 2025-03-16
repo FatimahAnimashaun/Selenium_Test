fc3263ef7e9248ff36c5b6139a93cbb764d283eb
fatimahanimashaun <fatimahanimashaun@womentechsters.org>
2025-03-16 01:50:26 +0100

    Initial commit

---
+++ First_test.py
@@ -0,0 +1,13 @@
+import time
+
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+driver.maximize_window()
+
+driver.find_element(By.ID, "user-name").send_keys("standard_user")
+driver.find_element(By.ID, "password").send_keys("secret_sauce")
+driver.find_element(By.ID, "login-button").click()
+time.sleep(5)
